fc224160c4f31cf257b3b8f9c664559767a81935
JulieHwang <67768114+julie9312@users.noreply.github.com>
2020-07-26 05:25:15 +0900

    Update ex2.js

--- example/ex2.js
+++ example/ex2.js
@@ -3,10 +3,10 @@ let mysql = require('mysql')
 
 let connection = mysql.createConnection(
     {
-        host : 'database-1.c51w3te8io5h.ap-northeast-2.rds.amazonaws.com',
+        host : 'database-1........rds.amazonaws.com',
         user : 'node_user',
         database : 'my_test',
-        password : 'node1234test'
+        password : '.....'
     }
 );
 
